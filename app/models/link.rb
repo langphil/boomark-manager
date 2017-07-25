5f6dfe5625281352718e74db74fbb15a85183b78
Phil Lang <plangphoto@gmail.com>
2017-07-25 19:37:35 +0100

    updated database access

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,6 +12,6 @@ class Link
 end
 
 # The line below needs a databas to be created >psql >create database name;
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
